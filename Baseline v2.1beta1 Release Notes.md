b7df036a8ac8e2f3ca9fd43ba4b9795e8472c4cc
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2024-01-21 12:42:51 -0800

    Added subtitle support

---
+++ Baseline v2.1beta1 Release Notes.md
@@ -0,0 +1,9 @@
+# Baseline v2.1beta1
+- New Configuration Key: `Subtitle`
+    - Added support for the new subtitle feature in SwiftDialog 2.4. (#^^^ LINK HERE ^^^#)
+
+
+
+
+# Additional Goals
+- Move more features over to the json file, that way we don't run into shell length issues if someone puts a long `--message` or `--infobox` string
--- Baseline.sh
+++ Baseline.sh
@@ -6,7 +6,7 @@ set -x
 #   @BigMacAdmin on the MacAdmins Slack
 #   trevor@secondsonconsulting.com
 
-scriptVersion="v.2.0"
+scriptVersion="v.2.1beta1"
 
 ########################################################################################################
 ########################################################################################################
@@ -559,9 +559,17 @@ function build_dialog_array()
 			currentIconPath=""
 		fi
         
+		#Get the desired subtitle if populated in the configuration profile
+        if $pBuddy -c "Print :$configKey:${index}:Subtitle" "$BaselineConfig" > /dev/null 2>&1; then
+			currentSubtitle=$($pBuddy -c "Print :$configKey:${index}:Subtitle" "$BaselineConfig")
+		else
+			#If no icon key is set, ensure it's blank
+			currentSubtitle=""
+		fi
+        
         #Generate JSON entry for item
         #NOTE: We will strip out the final comma later to ensure a valid JSON
-        dialogListJson+="{\"title\" : \"$currentDisplayName\", \"icon\" : \"$currentIconPath\", \"status\" : \"\"},"
+        dialogListJson+="{\"title\" : \"$currentDisplayName\",\"subtitle\" : \"$currentSubtitle\", \"icon\" : \"$currentIconPath\", \"status\" : \"\"},"
 
         #Done looping. Increase our array value and loop again.
         index=$((index+1))
