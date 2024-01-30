c284c8d60d684faeb2cdc991947e98e341520348
sjurlohne <57708526+sjurlohne@users.noreply.github.com>
2024-01-30 12:50:10 +0100

    Function for silentMode variable

--- Baseline.sh
+++ Baseline.sh
@@ -1203,6 +1203,24 @@ function update_tracker(){
     fi
 }
 
+function check_silent_option()
+{
+    silentModeEnabled=$($pBuddy -c "Print :SilentMode" "$BaselineConfig" 2> /dev/null )
+
+    if [ -z $silentModeEnabled ]; then
+        log_message "No SilentMode key in configuration file"
+        silentModeEnabled="false"
+    elif [[ "$silentModeEnabled" == "true" ]]; then
+        log_message "SilentMode set to true from configuration file"
+        silentModeEnabled=true
+    elif [[ "$silentModeEnabled" == "false" ]]; then
+        log_message "SilentMode set to false from configuration file"
+        silentModeEnabled="false"
+    else
+        log_message "Invalid value for SilentMode key. Setting to default. Invalid Key Value: $silentModeEnabled"
+        silentModeEnabled="false"
+    fi
+}
 
 ########################################################################################################
 ########################################################################################################
